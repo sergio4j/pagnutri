80db719414a5f3a94166d0ce3f169f8c5a4501eb
Sergio Rodríguez Siles <sergio4j@hotmail.com>
2020-04-23 13:59:58 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+document.querySelector('.menu-btn').addEventListener('click' ,() =>{
+    document.querySelector('.nav-menu').classList.toggle('show')
+});
+
+ScrollReveal().reveal('.showcase');
+
+ScrollReveal().reveal('.noticias', {delay:500 });
+
+ScrollReveal().reveal('.car-baner2', {delay:500 });
+
+ScrollReveal().reveal('.social', {delay:500 });
+
